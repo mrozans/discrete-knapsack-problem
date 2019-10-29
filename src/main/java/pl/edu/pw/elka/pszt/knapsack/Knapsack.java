875ec69f3bd16bd4cd5e2c66a7daf6e225aacb37
marcin <293160@pw.edu.pl>
2019-10-29 10:41:01 +0100

    features/save result

--- src/main/java/pl/edu/pw/elka/pszt/knapsack/Knapsack.java
+++ src/main/java/pl/edu/pw/elka/pszt/knapsack/Knapsack.java
@@ -5,35 +5,42 @@ import pl.edu.pw.elka.pszt.knapsack.model.InputKnapsackObjects;
 import pl.edu.pw.elka.pszt.knapsack.model.InputLoader;
 import pl.edu.pw.elka.pszt.knapsack.model.ValidateInputKnapsackObjects;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 @AllArgsConstructor
 public class Knapsack {
-    private final String inputPath;
-    private final String outputPath;
+    private final String inputPath, outputPath;
 
     public void run() throws IOException {
         InputKnapsackObjects iko = loadInput();
         validate(iko);
-        calculate(iko);
-        //ToDo save output
-    }
-
-    private void validate(InputKnapsackObjects iko) throws IOException {
-        if(ValidateInputKnapsackObjects.checkCapacity(iko))
-            throw new IOException("Error in capacity. Capacity must be integer >= 0, but is: " + iko.getKnapsackCapacity());
-        if(ValidateInputKnapsackObjects.checkItems(iko))
-            throw new IOException("Error in items. Value and weight for each item must be integer. All items: " +
-                    Arrays.toString(iko.getKnapsackObjects().toArray()));
+        String result = calculate(iko);
+        saveOutput(result);
     }
 
     private InputKnapsackObjects loadInput() throws IOException {
         return new InputLoader(inputPath).load();
     }
 
-    private void calculate(InputKnapsackObjects iko) {
+    private void validate(InputKnapsackObjects iko) throws IOException {
+        if (ValidateInputKnapsackObjects.checkCapacity(iko))
+            throw new IOException("Error in capacity. Capacity must be integer >= 0, but is: " + iko.getKnapsackCapacity());
+        if (ValidateInputKnapsackObjects.checkItems(iko))
+            throw new IOException("Error in items. Value and weight for each item must be integer. All items: " +
+                    Arrays.toString(iko.getKnapsackObjects().toArray()));
+    }
+
+    private String calculate(InputKnapsackObjects iko) {
         //ToDo body;
+        return null;
+    }
+
+    private void saveOutput(String string) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
+        writer.write(string);
+        writer.close();
     }
 }
